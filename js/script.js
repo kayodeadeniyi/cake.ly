87410ae4b08a6ef0696728284a15f566f7674804
Kayode Adeniyi <kayode.adeniyi@andela.co>
2016-01-27 02:00:05 +0100

    Get image url from cloudinary Add flickity Add ImageLiquid

--- js/script.js
+++ js/script.js
@@ -1,3 +1,134 @@
+var seedData = {
+  cakes: [
+    {
+      name:'Chocolate Bazooka',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price:'1999.99',
+      imageSrc:'http://res.cloudinary.com/awiliuzo/image/upload/c_fill,w_100,h_100/v1453842919/home-bg_t1rosa.jpg'
+    },
+    {
+      name:'S Bazooka',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price:'1999.99',
+      imageSrc:'http://res.cloudinary.com/awiliuzo/image/upload/c_fill,w_100,h_100/v1453842919/Cake_Hd_Walpapers-007_cyi2o3.jpg'
+    },
+    {
+      name:'Chocolate Bazooka',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price:'1999.99',
+      imageSrc:'http://res.cloudinary.com/awiliuzo/image/upload/c_fill,w_100,h_100/v1453842919/amazingly_delicious_cake_aece2d.jpg'
+    },
+    {
+      name:'Chocolate Bazooka',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price:'1999.99',
+      imageSrc:'http://res.cloudinary.com/awiliuzo/image/upload/c_fill,w_100,h_100/v1453842919/home-bg_t1rosa.jpg'
+    },
+    {
+      name:'S Bazooka',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price:'1999.99',
+      imageSrc:'http://res.cloudinary.com/awiliuzo/image/upload/c_fill,w_100,h_100/v1453842919/Cake_Hd_Walpapers-007_cyi2o3.jpg'
+    },
+    {
+      name:'Chocolate Bazooka',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price:'1999.99',
+      imageSrc:'http://res.cloudinary.com/awiliuzo/image/upload/c_fill,w_100,h_100/v1453842919/amazingly_delicious_cake_aece2d.jpg'
+    },
+    {
+      name:'Chocolate Bazooka',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price:'1999.99',
+      imageSrc:'http://res.cloudinary.com/awiliuzo/image/upload/c_fill,w_100,h_100/v1453842919/home-bg_t1rosa.jpg'
+    },
+    {
+      name:'S Bazooka',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price:'1999.99',
+      imageSrc:'http://res.cloudinary.com/awiliuzo/image/upload/c_fill,w_100,h_100/v1453842919/Cake_Hd_Walpapers-007_cyi2o3.jpg'
+    },
+    {
+      name:'Chocolate Bazooka',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price:'1999.99',
+      imageSrc:'http://res.cloudinary.com/awiliuzo/image/upload/c_fill,w_100,h_100/v1453842919/amazingly_delicious_cake_aece2d.jpg'
+    },
+    {
+      name:'Chocolate Bazooka',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price:'1999.99',
+      imageSrc:'http://res.cloudinary.com/awiliuzo/image/upload/c_fill,w_100,h_100/v1453842919/home-bg_t1rosa.jpg'
+    },
+    {
+      name:'S Bazooka',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price:'1999.99',
+      imageSrc:'http://res.cloudinary.com/awiliuzo/image/upload/c_fill,w_100,h_100/v1453842919/Cake_Hd_Walpapers-007_cyi2o3.jpg'
+    },
+    {
+      name:'Chocolate Bazooka',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price:'1999.99',
+      imageSrc:'http://res.cloudinary.com/awiliuzo/image/upload/c_fill,w_100,h_100/v1453842919/amazingly_delicious_cake_aece2d.jpg'
+    },
+    {
+      name:'Chocolate Bazooka',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price:'1999.99',
+      imageSrc:'http://res.cloudinary.com/awiliuzo/image/upload/c_fill,w_100,h_100/v1453842919/home-bg_t1rosa.jpg'
+    },
+    {
+      name:'S Bazooka',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price:'1999.99',
+      imageSrc:'http://res.cloudinary.com/awiliuzo/image/upload/c_fill,w_100,h_100/v1453842919/Cake_Hd_Walpapers-007_cyi2o3.jpg'
+    },
+    {
+      name:'Chocolate Bazooka',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price:'1999.99',
+      imageSrc:'http://res.cloudinary.com/awiliuzo/image/upload/c_fill,w_100,h_100/v1453842919/amazingly_delicious_cake_aece2d.jpg'
+    },
+    {
+      name:'Chocolate Bazooka',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price:'1999.99',
+      imageSrc:'http://res.cloudinary.com/awiliuzo/image/upload/c_fill,w_100,h_100/v1453842919/home-bg_t1rosa.jpg'
+    },
+    {
+      name:'S Bazooka',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price:'1999.99',
+      imageSrc:'http://res.cloudinary.com/awiliuzo/image/upload/c_fill,w_100,h_100/v1453842919/Cake_Hd_Walpapers-007_cyi2o3.jpg'
+    },
+    {
+      name:'Chocolate Bazooka',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price:'1999.99',
+      imageSrc:'http://res.cloudinary.com/awiliuzo/image/upload/c_fill,w_100,h_100/v1453842919/amazingly_delicious_cake_aece2d.jpg'
+    },
+    {
+      name:'Chocolate Bazooka',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price:'1999.99',
+      imageSrc:'http://res.cloudinary.com/awiliuzo/image/upload/c_fill,w_100,h_100/v1453842919/home-bg_t1rosa.jpg'
+    },
+    {
+      name:'S Bazooka',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price:'1999.99',
+      imageSrc:'http://res.cloudinary.com/awiliuzo/image/upload/c_fill,w_100,h_100/v1453842919/Cake_Hd_Walpapers-007_cyi2o3.jpg'
+    },
+    {
+      name:'Chocolate Bazooka',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      price:'1999.99',
+      imageSrc:'http://res.cloudinary.com/awiliuzo/image/upload/c_fill,w_100,h_100/v1453842919/amazingly_delicious_cake_aece2d.jpg'
+    }
+
+  ]
+}
 
 var navbar = {
   $element: $('.ck-navbar'),
@@ -7,7 +138,7 @@ var navbar = {
   }
 }
 
-var hero = {
+hero = {
   $browseBtn: $('.ck-hero-content button'),
   $baseElement: $('.ck-hero'),
 
@@ -55,8 +186,71 @@ var sideBar = {
   }
 }
 
+var flicker = {
+  $carouselMain: $('.ck-main-carousel'),
+  $carouselThumb: $('.ck-thumb-carousel'),
+  thumbItemSample: '<div class="carousel-item imgFilled"><img src="#"/><div class="overlay"></div></div>',
+
+  init: function() {
+    //populate the galleries:
+    for (var i = 0; i < 21; i++ ) {
+      var cake = seedData.cakes[i]
+      var carouselItem = $(this.thumbItemSample)
+      var carouselItem2 = $(this.thumbItemSample)
+      var largeImg = cake.imageSrc.split('/c_fill,w_100,h_100').join('')
+      carouselItem.find('img').attr('src',cake.imageSrc)
+      this.$carouselThumb.append(carouselItem)
+      carouselItem2.find('img').attr('src',largeImg)
+      this.$carouselMain.append(carouselItem2)
+    }
+
+    // initialize the galleries with flickity
+    flickityConfig = {
+        cellSelector: '.carousel-item',
+        setGallerySize: false,
+        freeScroll: true,
+        wrapAround: true,
+        autoPlay: true,
+        imagesLoaded: true,
+        pageDots: false,
+        prevNextButtons: false
+    }
+    var carouselThumb = this.$carouselThumb.flickity(flickityConfig)
+    flickityConfig.autoPlay = false
+    $carouselMain = this.$carouselMain.flickity(flickityConfig)
+
+    this.$carouselThumb.on( 'cellSelect', function() {
+      console.log( 'Flickity select ' + carouselThumb.selectedIndex )
+    })
+  }
+}
+
+var imageLiquid = {
+  config: {
+    filled: {
+      fill: true,
+      horizontalAlign: "center",
+      verticalAlign: "center"
+    },
+    fitted: {
+      fill: false,
+      horizontalAlign: "center",
+      verticalAlign: "center"
+    }
+  },
+
+  init: function(customConfig){
+    // intializes the plugin on images:
+    $('.imgFilled').imgLiquid(imageLiquid.config.filled)
+    $('.imgFitted').imgLiquid(imageLiquid.config.fitted)
+  }
+}
+
 $(document).ready(function(){
   hero.init()
   carousels.init()
   cover.init()
+  flicker.init()
+  imageLiquid.init()
+
 })
